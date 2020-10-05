8d7213bdc23615d73da60be0b9f80f97f41a99cf
Kononovich Halina <kononovich1703@gmail.com>
2020-10-05 18:36:59 +0300

    use react.cloneElement

--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -3,11 +3,11 @@ import './person-details.css';
 import SwapiService from '../../services/swapi-service'; 
 import ErrorButton from '../error-button';
 
-const Record = ({field , label}) => {
+const Record = ({item, field , label}) => {
  return ( 
  <li className="list-group-item">
     <span className="term">{label}</span>
-    <span>{field}</span>
+    <span>{item[field]}</span>
   </li>
   );
 }
@@ -55,7 +55,7 @@ export default class ItemDetails extends Component {
     }
 
     const {id, name, gender, birthYear, eyecolor} = this.state.item;
-    const {image} = this.state;
+    const {image, item} = this.state;
 
     return (
       <div className="item-details card">
@@ -67,7 +67,7 @@ export default class ItemDetails extends Component {
           <ul className="list-group list-group-flush">
             {
               React.Children.map(this.props.children, (child) => {
-                return child;
+                return React.cloneElement(child, {item});
               })
             }
           </ul>
