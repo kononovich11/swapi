e1f8861e0cd54f79747692f068b70913a86c261d
Kononovich Halina <kononovich1703@gmail.com>
2020-10-07 18:06:18 +0300

    work with propTypes

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -11,6 +11,16 @@ export default class RandomPlanet extends Component {
     updateInterval: 7000,
   };
 
+  static propTypes = {
+    updateInterval: (props, propName, componentName) => {
+      const value = props[propName];
+      if(typeof value === 'number' && !isNaN(value))  {
+        return null;
+      };
+      return new TypeError(`${componentName}: ${propName} must be number`);
+    }
+  }
+
   state = {
     planet: {},
     loader: true,
