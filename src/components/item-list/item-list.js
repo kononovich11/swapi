2193028d16af0c94a86a1890b060e8996cf5cabb
Kononovich Halina <kononovich1703@gmail.com>
2020-10-07 18:34:49 +0300

    work with prop-types library

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './item-list.css';
 
 const ItemList = (props) => {
@@ -26,6 +27,11 @@ const ItemList = (props) => {
 
 ItemList.defaultProps = {
   onItemSelected: () => {},
+};
+
+ItemList.propTypes = {
+  onItemSelected: PropTypes.func,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default ItemList;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './random-planet.css';
 import SwapiService from '../../services/swapi-service';
@@ -12,13 +13,7 @@ export default class RandomPlanet extends Component {
   };
 
   static propTypes = {
-    updateInterval: (props, propName, componentName) => {
-      const value = props[propName];
-      if(typeof value === 'number' && !isNaN(value))  {
-        return null;
-      };
-      return new TypeError(`${componentName}: ${propName} must be number`);
-    }
+    updateInterval: PropTypes.number,
   }
 
   state = {
